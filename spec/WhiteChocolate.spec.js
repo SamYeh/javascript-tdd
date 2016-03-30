85efeedc40e1d078ab9bc17eb17e59b9b576a540
Sam Yeh <hyeh@expedia.com>
2016-03-30 13:34:03 -0700

    restructuring the tests

--- spec/WhiteChocolate.spec.js
+++ spec/WhiteChocolate.spec.js
@@ -1,19 +1,17 @@
-describe("Test for WhiteChocolate", function () {
+define(["lib/lodash", "src/WhiteChocolate"], function(_, WhiteChocolate) {
     "use strict";
-    define(["lib/lodash", "src/WhiteChocolate"], function(_, WhiteChocolate) {
-        describe("WhiteChocolate", function () {
-            it("is a function", function () {
-                expect(_.isFunction(WhiteChocolate)).toBe(true);
-            });
+    describe("WhiteChocolate", function () {
+        it("is a function", function () {
+            expect(_.isFunction(WhiteChocolate)).toBe(true);
         });
-        describe("an instance of WhiteChocolate", function () {
-            var myChocolate = new WhiteChocolate();
-            it("is an object", function () {
-                expect(_.isObject(myChocolate)).toBe(true);
-            });
-            it("is white", function () {
-                expect(myChocolate.color).toBe("white");
-            });
+    });
+    describe("an instance of WhiteChocolate", function () {
+        var myChocolate = new WhiteChocolate();
+        it("is an object", function () {
+            expect(_.isObject(myChocolate)).toBe(true);
+        });
+        it("is white", function () {
+            expect(myChocolate.color).toBe("white");
         });
     });
 });
