359ed6c705cccc159c4451a33d4096c6fbb28c27
Sam Yeh <hyeh@expedia.com>
2016-03-30 13:21:13 -0700

    minimal set of files to do TDD in JavaScript

---
+++ spec/WhiteChocolate.spec.js
@@ -0,0 +1,19 @@
+describe("Test for WhiteChocolate", function () {
+    "use strict";
+    define(["lib/lodash", "src/WhiteChocolate"], function(_, WhiteChocolate) {
+        describe("WhiteChocolate", function () {
+            it("is a function", function () {
+                expect(_.isFunction(WhiteChocolate)).toBe(true);
+            });
+        });
+        describe("an instance of WhiteChocolate", function () {
+            var myChocolate = new WhiteChocolate();
+            it("is an object", function () {
+                expect(_.isObject(myChocolate)).toBe(true);
+            });
+            it("is white", function () {
+                expect(myChocolate.color).toBe("white");
+            });
+        });
+    });
+});
---
+++ src/WhiteChocolate.js
@@ -0,0 +1,7 @@
+define(function () {
+    "use strict";
+    function WhiteChocolate() {
+        this.color = "white";
+    }
+    return WhiteChocolate;
+});
